d4eb7acbbf6b72d1ae81c3f8b4987259eac735cd
daizygod <ruslansubway@mail.ru>
2022-09-12 13:27:20 +0300

    added select2 and jquery, now update and crete working normal, and some fixes

--- resources/views/Tracks/form.blade.php
+++ resources/views/Tracks/form.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Shark App</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
+@extends('layout')
+
+@section('content')
 <body>
 <div class="container">
 
@@ -15,6 +12,29 @@
             <li><a href="{{ URL::to('tracks') }}">View All tracks</a></li>
         </ul>
     </nav>
+    <script>
+        $( document ).ready(function() {
+            $("#artist_id").select2({
+                ajax: {
+                    type: "get",
+                    url: "{{route('getAjax')}}",
+                    data: function(params) {
+                        return {
+                            search: params.term
+                        }
+                    },
+                    dataType: 'json',
+                    processResults: function (response) {
+                        return {
+                            results: response
+                        };
+                    },
+                    cache: true
+                    // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+                }
+            });
+        });
+    </script>
     {!! Form::open([
         'url' => isset($track) ? route('tracks.update', $track) : route('tracks.store'),
         'method' => 'post',
@@ -30,14 +50,21 @@
     <div style="justify-content: space-between; height: 60em;margin: unset; width: 22em; display: flex; flex-direction: column; align-items: flex-start;">
     {!! Form::label('name', null, ['class' => 'control-label']) !!}
     {!! Form::text('name', isset($track->name) ? $track->name : null, ['class' => 'form-control']); !!}
+    {!! Form::label('artist', null, ['class' => 'control-label']) !!}
     {!!
-        Form::select('artist_id', \App\Models\Artist::select('artists.name')
+        Form::select('artist_id', isset($track->artist_id) ? \App\Models\Artist::where(['id' => $track->artist_id])->pluck('name')->toArray()
+        : \App\Models\Artist::select('artists.name')
         ->leftjoin('tracks', 'tracks.artist_id', '=', 'artists.id')
         ->groupBy('artists.name', 'artists.id')
         ->orderByRaw('SUM(tracks.counter) DESC')
         ->pluck('name')
         ->take(10)
-        ->toArray(), (isset($track->artist_id) && $track->artist_id != 0) ? $track->artist_id : 1, ['class' => 'form-control']);
+        ->toArray(), (isset($track->artist_id) && $track->artist_id != 0) ? $track->artist_id : 1,
+        [
+            'class' => 'form-control',
+            'id' => 'artist_id',
+            'multiple' => false
+        ]);
         //TODO: need to fix on update select (selecting not by artist_id u know)
     !!}
 
@@ -45,8 +72,10 @@
     {!! Form::date('release_date', isset($track->release_date) ? $track->release_date : null, ['class' => 'form-control']); !!}
         {!! Form::label('type', null, ['class' => 'control-label']) !!}
     {!! Form::select('type', \App\Models\Track::$types_array, isset($track->type) ? $track->type : 1, ['class' => 'form-control']); !!}
+    <?php if (!isset($track->id)) { ?>
         {!! Form::label('counter', null, ['class' => 'control-label']) !!}
-    {!! Form::number('counter', isset($track->counter) ? $track->counter : 1, ['class' => 'form-control']); !!}
+        {!! Form::number('counter', isset($track->counter) ? $track->counter : 1, ['class' => 'form-control']); !!}
+    <?php } ?>
         {!! Form::label('photo_cover_id', null, ['class' => 'control-label']) !!}
     {!! Form::number('photo_cover_id', isset($track->photo_cover_id) ? $track->photo_cover_id : 1, ['class' => 'form-control']); !!}
         {!! Form::label('file_id', null, ['class' => 'control-label']) !!}
@@ -63,4 +92,4 @@
 
 </div>
 </body>
-</html>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
