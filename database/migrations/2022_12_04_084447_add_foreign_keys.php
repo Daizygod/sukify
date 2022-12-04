385a707261baec1787663812577971ee0328aa5c
daizygod <ruslansubway@mail.ru>
2022-12-05 00:36:20 +0300

    add foreign to artist_id in table tracks

---
+++ database/migrations/2022_12_04_084447_add_foreign_keys.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tracks', function (Blueprint $table)
+        {
+            $table->dropColumn('artist_id');
+        });
+
+        Schema::table('tracks', function (Blueprint $table)
+        {
+            $table->foreignId('artist_id')
+                ->constrained('artists')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tracks', function (Blueprint $table)
+        {
+            $table->dropColumn('artist_id');
+        });
+
+        Schema::table('tracks', function (Blueprint $table)
+        {
+            $table->integer('artist_id');
+        });
+    }
+};
