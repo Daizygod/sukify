1628fc1a5bce3fd278152b5204f4feae700912bc
daizygod <ruslansubway@mail.ru>
2023-08-16 18:57:42 +0300

    swagger

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('swagger', 'swagger');
 
 Route::resource('player', PlayerController::class);
 
