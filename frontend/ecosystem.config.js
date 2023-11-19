609e39040f9be7739f8cad5afd6b2a30e24b78ce
el1xz <glaiber2010@gmail.com>
2023-11-19 17:42:06 +0300

    update pm2

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: "Sukify",
+      port: "3000",
       exec_mode: "cluster",
-      instances: "max", // Or a number of instances
-      script: "./node_modules/nuxt/bin/nuxt.js",
-      args: "start",
+      instances: "max",
+      script: ".output/server/index.mjs",
     },
   ],
 };
