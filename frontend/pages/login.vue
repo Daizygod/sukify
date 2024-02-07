e2f72d3b204d3275f7f69e75695fa84a577d6078
el1xz <glaiber2010@gmail.com>
2024-02-07 12:24:41 +0300

    update (add auth)

---
+++ frontend/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-if="!isAuthenticated">
+      <button @click="login({ email: 'test@test.com', password: 'qwerty123' })">
+        Login
+      </button>
+    </div>
+    <div v-else>
+      <button @click="logout">Logout</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useAuth } from "@/composables/useAuth";
+
+const { isAuthenticated, login, logout } = useAuth();
+</script>
